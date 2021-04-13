f267bd41e68e4052428b19b08798bf5237ef686b
Zanshin9 <rylander.j92@gmail.com>
2021-04-13 09:07:16 +0200

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6cb0bdb1-ba37-4847-8f61-8a0298b8166f';
+export const MODAL_CLOSE_SEC = 2.5;
